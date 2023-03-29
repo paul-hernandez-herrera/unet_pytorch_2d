df0c212b841226530ac57ba4fb09705639aa56a2
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-03-29 17:55:26 -0600

    Refactoring code: improving readibility and usability

--- core_code/UNet_2D/data_augmentation.py
+++ core_code/UNet_2D/data_augmentation.py
@@ -3,58 +3,57 @@ import numpy as np
 
 class augmentation_segmentation_task():
     def __init__(self):
-        #nothing to initialize
         self.zoom_range = [0.8, 1.2]
         self.shear_angle = [-5, 5]
         
         #flag to compute specific transformations
-        self.shear_flag = True
-        self.hflip_flag = True
-        self.vflip_flag = True
-        self.zoom_flag = True
-        return
+        self.enable_shear = True
+        self.enable_hflip = True
+        self.enable_vflip = True
+        self.enable_zoom = True
         
     def horizontal_flip(self, image, target):        
         #random horizontal flip
         if np.random.uniform(0, 1) > 0.5:
-            image = hflip(image)
-            target = hflip(target)
+            image, target = hflip(image), hflip(target)
         return image, target
     
     def vertical_flip(self, image, target):        
         #random vertical flip    
         if np.random.uniform(0, 1) > 0.5:
-            image = vflip(image)
-            target = vflip(target)
+            image, target = vflip(image), vflip(target)
         return image, target 
+    
+    def affine_transform(self, image, target, scale=1, angle=0, translate=[0, 0], shear=0):
+        image = affine(image, scale=scale, angle=angle, translate=translate, shear=shear)
+        target = affine(target, scale=scale, angle=angle, translate=translate, shear=shear)
+        return image, target    
     
     def affine_zoom(self, image, target):        
         #random zoom
         if np.random.uniform(0, 1) > 0.5:
-            zoom = np.random.uniform(low= self.zoom_range[0], high= self.zoom_range[1])
-            image = affine(image, scale = zoom, angle = 0, translate = [0,0], shear = 0)
-            target = affine(target, scale = zoom, angle = 0, translate = [0,0], shear = 0)
+            zoom = np.random.uniform(*self.zoom_range)
+            image, target = self.affine_transform(image, target, scale=zoom)
         return image, target
     
     def affine_shear(self, image, target):        
         #random shear
         if np.random.uniform(0, 1) > 0.5:
-            shear = np.random.uniform(low= self.shear_angle[0], high= self.shear_angle[1])
-            image = affine(image, scale = 1, angle = 0, translate = [0,0], shear = shear)
-            target = affine(target, scale = 1, angle = 0, translate = [0,0], shear = shear)
+            shear = np.random.uniform(*self.shear_angle)
+            image, target = self.affine_transform(image, target, shear=shear)
         return image, target
         
     def run(self, image, target):
-        if self.hflip_flag:
+        if self.enable_hflip:
             image, target = self.horizontal_flip(image, target)
             
-        if self.vflip_flag:
+        if self.enable_vflip:
             image, target = self.vertical_flip(image, target)
             
-        if self.zoom_flag:
+        if self.enable_zoom:
             image, target = self.affine_zoom(image, target)
             
-        if self.shear_flag:
+        if self.enable_shear:
             image, target = self.affine_shear(image, target)
         
         return image, target   
