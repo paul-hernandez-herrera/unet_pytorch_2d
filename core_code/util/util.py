24b72f3c2518e6314ee605e9dd86e3ef5d53dba4
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-04-13 22:25:51 -0600

    moving functions to display images to the python file core_code/util/show_image.py

--- core_code/util/util.py
+++ core_code/util/util.py
@@ -1,7 +1,6 @@
 import tifffile
 from pathlib import Path
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 def imread(filename):
     if Path(filename).suffix in {'.tif', '.tiff'}:
@@ -33,27 +32,5 @@ def create_file_in_case_not_exist(folder_path):
     folder_path.mkdir(parents=True, exist_ok=True)
     return
 
-def show_images_from_Dataset(custom_dataset, n_images_to_display=3):
-    img, _  = custom_dataset[0]
-    n_channels = img.shape[0]
 
-    # Create figure with subplots for each image and its channels
-    fig, axs = plt.subplots(n_images_to_display, n_channels+1, figsize=(10, 20), dpi=80)
-    for i in range(n_images_to_display):
-        img, target = custom_dataset.__getitem__(np.random.randint(0, len(custom_dataset)))
-        img = img.numpy()
-        target = target.numpy()
-        for j in range(n_channels):
-            axs[i,j].imshow(np.squeeze(img[j,:,:]), cmap='gray')
-            axs[i,j].set_title('Image ' + str(i) + "--- Ch" + str(j))
-            axs[i,j].axis('off')
-            fig.colorbar(axs[i,j].imshow(np.squeeze(img[j,:,:]), cmap='gray'), ax=axs[i,j])
-            
-        for ch in range(target.shape[0]):
-            target[ch,:,:] = (ch+1)*target[ch,:,:]            
-        axs[i,n_channels].imshow(target.max(axis=0), cmap='gray')
-        axs[i,n_channels].set_title('Ground truth (target)')
-        axs[i,n_channels].axis('off')
-        fig.colorbar(axs[i,n_channels].imshow(target.max(axis=0), cmap='gray'), ax=axs[i,n_channels])
-    plt.tight_layout()
-    plt.show()       
+    
