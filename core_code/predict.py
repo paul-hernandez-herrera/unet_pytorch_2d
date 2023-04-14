f037ae1c80bb4cb75c6c454724a297c75c2a7f62
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-04-13 22:45:21 -0600

    refactoring function

--- core_code/predict.py
+++ core_code/predict.py
@@ -3,6 +3,8 @@ import numpy as np
 from pathlib import Path
 import torch
 from .util.deeplearning_util import load_model
+from .parameters_interface import ipwidget_basic
+from .parameters_interface.parameters_widget import parameters_device
 
 def predict_model(input_path, model= None, model_path = None, output_folder=None, device = 'cpu'):
     file_paths = util.get_image_file_paths(input_path)
@@ -35,6 +37,24 @@ def predict_model(input_path, model= None, model_path = None, output_folder=None
             output_file_paths.append(output_file_path)
     return {"inputs": file_paths, "outputs": output_file_paths}
 
+
+class PredictSegmentationInteractive:
+    def __init__(self):
+        #setting the parameters required to predict images
+        # Set parameters required to predict images
+        self.model_path_w = ipwidget_basic.set_text('Model path:', 'Insert path here')
+        self.folder_path_w = ipwidget_basic.set_text('Input path:', 'Insert path here')
+        self.folder_output_w = ipwidget_basic.set_text('Output path:', 'Insert path here')
+        self.device_w = parameters_device()
+        
+    def run(self):
+        model_path = self.model_path_w.value
+        device = self.device_w.get_device()
+    
+        # Predict images and return list of output file paths
+        file_paths = predict_model(self.folder_path_w.value, model_path = model_path, folder_output = self.folder_output_w.value, device = device)
+        
+        return file_paths
 
 if __name__== '__main__':
     
