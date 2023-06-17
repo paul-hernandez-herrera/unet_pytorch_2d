2bacf2d8b4ef8f776a17206047e052d85d24d1f9
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-06-17 17:25:54 -0600

    adding function calculate_test_performance, get_dataloader_file_names. Adding functionality to add losses or multiply them.

--- core_code/util/deeplearning_util.py
+++ core_code/util/deeplearning_util.py
@@ -4,10 +4,13 @@ import torch.nn.functional as F
 from pathlib import Path
 from datetime import datetime
 from ..models.UNet2d_model import Classic_UNet_2D
+from ..loss.binary_loss import BinaryLoss
+from torch.utils.data.dataset import Subset
+from ..util import util
+import numpy as np
 
 
-
-def train_one_epoch(model, train_loader, optimizer, loss_functions, device):
+def train_one_epoch(model, train_loader, optimizer, loss_functions, device, loss_reduction = 'sum'):
     #This is the main code responsible for updating the weights of the model for a single epoch
     
     model.train() #set the model in training mode
@@ -27,7 +30,11 @@ def train_one_epoch(model, train_loader, optimizer, loss_functions, device):
         network_output = model(imgs) 
         
         # Compute the loss
-        loss = sum([f(network_output, targets) for f in loss_functions]) # compute the error between the network output and target output
+        if loss_reduction=='sum':
+            loss = sum([f(network_output, targets) for f in loss_functions]) # compute the error between the network output and target output
+        elif loss_reduction=='prod':
+            losses = [loss_fn(network_output, targets) for loss_fn in loss_functions]
+            loss = torch.prod(torch.stack(losses)) # compute the product of losses
         
         # Backward pass
         loss.backward() # compute the gradients given the loss value
@@ -36,11 +43,11 @@ def train_one_epoch(model, train_loader, optimizer, loss_functions, device):
         optimizer.step() # update the weights of models using the gradients and the given optimizer
         
         epoch_loss += loss.item()
-    epoch_loss /= len(train_loader.dataset)
+    epoch_loss /= (len(loss_functions)*len(train_loader.dataset))
         
     return epoch_loss
 
-def calculate_validation_loss(model, validation_loader, loss_functions, device):
+def calculate_validation_loss(model, validation_loader, loss_functions, device, loss_reduction = 'sum'):
     model.eval() #set the model in evaluation mode
     val_loss = 0
     with torch.no_grad():  # disable gradient calculations during evaluation
@@ -53,12 +60,68 @@ def calculate_validation_loss(model, validation_loader, loss_functions, device):
             
             network_output = model(imgs) #applying the model to the input images
             
-            loss = sum([loss_fn(network_output, targets) for loss_fn in loss_functions]) # compute the error between the network output and target output
+            # Compute the loss
+            if loss_reduction=='sum':
+                loss = sum([f(network_output, targets) for f in loss_functions]) # compute the error between the network output and target output
+            elif loss_reduction=='prod':
+                losses = [loss_fn(network_output, targets) for loss_fn in loss_functions]
+                loss = torch.prod(torch.stack(losses)) # compute the product of losses               
             
             val_loss += loss.item()
         val_loss /= len(validation_loader.dataset)
         
     return val_loss
+
+def calculate_test_performance(model, test_loader, device, folder_output = None):
+    model.eval() #set the model in evaluation mode
+    
+    if not(folder_output):
+        folder_output = Path(folder_output,'test_results')
+        folder_output.mkdir(parents=True, exist_ok=True)
+    
+    metric_dice = np.array([])
+    binary_loss = BinaryLoss(option = 'dice', segmentation = True)
+    img_id = 0
+    with torch.no_grad():  # disable gradient calculations during evaluation
+        for batch in test_loader: 
+            imgs, targets = batch #getting imgs and target output for current batch
+            
+            #we have a tensor in the validation_loader, move to device
+            imgs = imgs.to(device= device, dtype = torch.float32)
+            targets = targets.to(device= device, dtype = torch.float32)
+            
+            network_output = model(imgs) #applying the model to the input images
+            
+            for j in range(0, targets.shape[0]):
+                current_output = network_output[j,:,:,:][None]
+                current_target = targets[j,:,:,:][None]
+                
+                val = 1-binary_loss(current_output, current_target)
+                
+                metric_dice = np.hstack((metric_dice, val.cpu().numpy()))
+                
+                # Calculate probability map and convert to numpy array
+                file_id = str(img_id).zfill(5)
+                img_id+=1
+                
+                probability = torch.sigmoid(current_output).squeeze().cpu().numpy()
+                util.imwrite(Path(folder_output, file_id + '_prob.tif'), (255 * probability).astype(np.uint8))
+                util.imwrite(Path(folder_output, file_id + '_img.tif'), (255*imgs[j,:,:,:]).cpu().numpy().astype(np.uint8))
+                util.imwrite(Path(folder_output, file_id + '_targets.tif'), (255 *targets[j,:,:,:]).cpu().numpy().astype(np.uint8))
+
+    w_dataset = test_loader.dataset.dataset
+    
+    index = np.array(test_loader.dataset.indices)
+        
+    test_set_len = len(test_loader)
+    print("Printing test set ")
+    k = 0
+    for i in index:
+        print(f'test img_id: {k} --- file_name: {w_dataset.file_names[i]}')
+        k+=1
+        
+    print(f"n_test = {test_set_len} ---- mean_error = {np.mean(metric_dice)} ---- std = {np.std(metric_dice)}" )
+    print(metric_dice)
 
 def get_model_outputdir(model_output_folder):
     if not model_output_folder:
@@ -129,6 +192,13 @@ def get_model(model_type, n_channels_input, n_channels_target):
     if model_type == 'unet_2d':
         model = Classic_UNet_2D(n_channels_input, n_channels_target)
         return model
+    
+def get_dataloader_file_names(dataset_loader, fullpath = True):
+    #index and file_names from test images
+    index = np.array(dataset_loader.dataset.indices)
+    file_names = [dataset_loader.dataset.dataset.file_names[i] if fullpath else dataset_loader.dataset.dataset.file_names[i].stem for i in index]
+    
+    return file_names
     
     
     
